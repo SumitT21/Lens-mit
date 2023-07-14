2f2afe816d57f671e6b123f829b685d4e6b9ebdd
SumitT21 <76245840+SumitT21@users.noreply.github.com>
2023-07-15 00:38:36 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //to change navigation styles on scroll
-alert("Please view in Desktop Mode")
+alert("Please view in Desktop Mode");
 window.addEventListener('scroll',()=>{
     document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY >0 );
 })
