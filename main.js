45963dfa97d68087e7c3a4bc5d4468052da3a6f9
SumitT21 <76245840+SumitT21@users.noreply.github.com>
2023-07-15 21:49:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
 //to change navigation styles on scroll
-alert("Please view in Desktop Mode");
+
 window.addEventListener('scroll',()=>{
     document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY >0 );
 })
+
+// circular word art in contact button
+
+const textButtons = document.querySelectorAll('.contact_btn');
+
+textButtons.forEach(textButtons => {
+    let text = textButtons.querySelector('p');
+    text.innerHTML = text.innerHTML.split('').map((character, index) => 
+    `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join(" ")
+})
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+  
