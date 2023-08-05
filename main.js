415f9c8da2589b5cfbe9fb44229152f704dbf79b
SumitT21 <76245840+SumitT21@users.noreply.github.com>
2023-08-05 17:45:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -15,13 +15,46 @@ textButtons.forEach(textButtons => {
 })
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints:{
+    599:{
+      slidesPerView:2,
+      spaceBetween:40
     },
-  });
+    1023:{
+      slidesPerView:3,
+      spaceBetween:60
+    }
+  }
+});
 
-
+const nav = document.querySelector(`.nav_links`);
+const openNavBtn = document.querySelector(`#nav_toggle-open`);
+const closeNavBtn = document.querySelector(`#nav_toggle-close`);
   
+const openNav = () => {
+  nav.style.display = 'flex';
+  openNavBtn.style.display = 'none';
+  closeNavBtn.style.display = 'inline-block';
+};
+
+openNavBtn.addEventListener('click', openNav);
+
+const closeNav = () => {
+  nav.style.display = 'none';
+  openNavBtn.style.display = 'inline-block';
+  closeNavBtn.style.display = 'none';
+};
+
+closeNavBtn.addEventListener('click', closeNav);
+
+//close menu on click on any menu list item
+nav.querySelectorAll('li a').forEach(navLink => {
+  navLink.addEventListener('click', closeNav);
+});
+
